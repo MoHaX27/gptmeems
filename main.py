28acf2d66a2af666051acb9fd2a9ab6b36f3e924
MoHaX27 <35339383+MoHaX27@users.noreply.github.com>
2025-08-14 23:39:03 +0400

    Use DefaultBotProperties for parse mode

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from __future__ import annotations
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 
 from bot.handlers import router
 from config.config import BOT_TOKEN
 
 
 async def main() -> None:
-    bot = Bot(BOT_TOKEN, parse_mode="HTML")
+    bot = Bot(BOT_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
